7fb0aaa11a75b743494f90fef6fe8a4f92600383
Tudda <trezzt0224@gmail.com>
2023-03-23 18:34:58 -0500

    Dont save this

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '37319f6c92msh9c130a5b873f9ccp137b95jsn6a0c24e2a0c1',
-		'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-	}
-};
-
-fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-    console.log(fetch)
-
-    
-$(document).ready(function () {
-    
-    $("#searchBtn").on("click", function () {
-
-        var searchTerm = $("#movieNameSearch").val();
-
-        $("movieNameSearch").val("");
-        
-        
-    })
-})
-=======
 const form = document.getElementById('book-search');
 const searchTermInput = document.getElementById('search-term');
 const searchResultsList = document.getElementById('search-results');
@@ -49,4 +21,3 @@ form.addEventListener('submit', (event) => {
     })
     .catch(error => console.error(error));
 });
->>>>>>> e8ffe2de4db0541d99b934671bc1e2b540b2119d
